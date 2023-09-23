b4c5f045e3224ce3a1b3f30a8816bc291473fd95
Adam Douglass <douglass@malloc.ca>
2023-09-23 15:50:58 -0400

    fix generic parameters

--- struct-metadata/src/lib.rs
+++ struct-metadata/src/lib.rs
@@ -18,31 +18,39 @@ use std::collections::HashMap;
 
 /// Information about a type along with its metadata and docstrings.
 #[derive(Debug, PartialEq, Eq)]
-pub struct Descriptor<Metadata=HashMap<&'static str, &'static str>> {
+pub struct Descriptor<Metadata: Default=HashMap<&'static str, &'static str>> {
     pub docs: Option<Vec<&'static str>>,
     pub metadata: Metadata,
-    pub kind: Kind,
+    pub kind: Kind<Metadata>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum Kind {
-    Struct { name: &'static str, children: Vec<Entry>, },
-    Aliased { name: &'static str, kind: Box<Descriptor> },
+pub enum Kind<Metadata: Default=HashMap<&'static str, &'static str>> {
+    Struct { name: &'static str, children: Vec<Entry<Metadata>>, },
+    Aliased { name: &'static str, kind: Box<Descriptor<Metadata>> },
+    Sequence( Box<Descriptor<Metadata>> ),
+    Mapping( Box<Descriptor<Metadata>>, Box<Descriptor<Metadata>> ),
     Bool,
     U64,
     String,
 }
 
 
-#[derive(Debug, PartialEq, Eq)]
-pub struct Entry {
+#[derive(Debug)]
+pub struct Entry<Metadata: Default=HashMap<&'static str, &'static str>> {
     pub label: &'static str,
     pub docs: Option<Vec<&'static str>>,
-    pub metadata: HashMap<&'static str, &'static str>,
-    pub type_info: Descriptor,
+    pub metadata: Metadata,
+    pub type_info: Descriptor<Metadata>,
 }
 
+impl<T: PartialEq + Default> PartialEq for Entry<T> {
+    fn eq(&self, other: &Self) -> bool {
+        self.label == other.label && self.docs == other.docs && self.metadata == other.metadata && self.type_info == other.type_info
+    }
+}
 
+impl<T: Eq + Default> Eq for Entry<T> {}
 
 pub trait Described<M: Default=HashMap<&'static str, &'static str>> {
     fn metadata() -> Descriptor<M>;
@@ -63,5 +71,25 @@ impl<M: Default> Described<M> for u64 {
 impl<M: Default> Described<M> for String {
     fn metadata() -> Descriptor<M> {
         Descriptor { docs: None, metadata: Default::default(), kind: Kind::String }
+    }
+}
+
+impl<M: Default, T: Described<M>> Described<M> for Vec<T> {
+    fn metadata() -> Descriptor<M> {
+        Descriptor { 
+            docs: None, 
+            metadata: Default::default(), 
+            kind: Kind::Sequence(Box::new(T::metadata())) 
+        }
+    }
+}
+
+impl<M: Default, K: Described<M> + core::hash::Hash, V: Described<M>> Described<M> for HashMap<K, V> {
+    fn metadata() -> Descriptor<M> {
+        Descriptor { 
+            docs: None, 
+            metadata: Default::default(), 
+            kind: Kind::Mapping(Box::new(K::metadata()), Box::new(V::metadata())) 
+        }
     }
 }
